5a20e86a3ec2fc0ae42302beb4445d02d7e43bea
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2025-05-22 23:58:23 +0300

    edit img family

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,7 @@
 import { Nunito } from "next/font/google";
-// import React, { Suspense } from "react";
 import cl from "./page.module.scss";
 import Header from "@/components/Header/Header";
 import { DEFAULT_SEO } from "@/util/seo.config";
-// import Loader from "@/components/Helper/Loader";
 import WrapperImg from "@/components/Wrapper/WrapperImg";
 
 export const nunito = Nunito({
@@ -26,10 +24,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="uk">
       <body className={`${nunito.variable} ${cl.wrapper}`}>
-        {/* <Suspense fallback={<Loader />}> */}
         <Header />
         <WrapperImg>{children}</WrapperImg>
-        {/* </Suspense> */}
       </body>
     </html>
   );
