b21e1ceb853ae62a7459c471b97c146155dc01a9
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2025-05-22 23:39:24 +0300

    fixed img in OpenGraph

--- src/util/seo.config.js
+++ src/util/seo.config.js
@@ -2,12 +2,7 @@ export const DEFAULT_SEO = {
   title: "Лелека медичний центр", // Базовое название сайта
   description:
     'Медичний центр "Лелека" в місті Калинівка - це сімейна медицина з турботою про вас і ваших близьких',
-  // openGraph: {
-  //   type: "website",
-  //   locale: "uk_UA",
-  //   url: "https://leleka.vercel.app", // Главный URL сайта
-  //   siteName: "МЦ Лелека", // Название сайта для Open Graph
-  // },
+
   openGraph: {
     title: "Лелека медичний центр",
     description:
