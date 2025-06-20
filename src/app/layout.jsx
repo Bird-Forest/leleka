15d14263119fb717ddf5b9d4dcfb82e1851fcc9e
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2025-06-20 11:48:30 +0300

    add GTM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,8 @@ import { Nunito } from "next/font/google";
 import cl from "./page.module.scss";
 import Header from "@/components/Header/Header";
 import { DEFAULT_SEO } from "@/util/seo.config";
+// import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import WrapperImg from "@/components/Wrapper/WrapperImg";
 
 export const nunito = Nunito({
@@ -23,10 +25,12 @@ export const metadata = DEFAULT_SEO;
 export default function RootLayout({ children }) {
   return (
     <html lang="uk">
+      <GoogleTagManager gtmId="GTM-N68R86G9" />
       <body className={`${nunito.variable} ${cl.wrapper}`}>
         <Header />
         <WrapperImg>{children}</WrapperImg>
       </body>
+      {/* <GoogleAnalytics gaId="G-XYZ" /> */}
     </html>
   );
 }
